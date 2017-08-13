ea65e90175b758738e06a5371211668f27929ae7
김동우 <idforcoding@gmail.com>
2017-08-13 11:56:41 +0000

    (fix) router-"/upload"

--- routes/index.js
+++ routes/index.js
@@ -55,73 +55,73 @@ router.post("/admin", function(req, res) {
     res.render("login");
   }
 });
-router.post("/upload/:file", function(req, res) {
-  if (req.params.file === "json") {
-    console.log("file")
+router.post("/upload", function(req, res) {
+  
+  console.log("file")
+  
+  var form = new formidable.IncomingForm();
+  form.uploadDir = __dirname + "/../upload";
+  // 파일 전송 시 첫번째 chunk가 전달되면 호출
+  form.on('fileBegin', function(name, file) {
+    console.log('fileBegin - ' + name + ':' + JSON.stringify(file));
+  })
+  // 파일의 chunk가 전달될 때 마다 호출
+  .on('progress', function(bytesReceived, bytesExpected) {
+    console.log('progress: ' + bytesReceived + '/' + bytesExpected);
+  })
+  // 파일 전송도중 esc나 페이지 전환 등으로 중단될 때 호출
+  .on('aborted', function() {
+    console.log('aborted');
+    res.render("error");
+  })
+  // error 발생 시 호출
+  .on('error', function() {
+    console.log('error');
+    res.render("error");
+  })
+  // 파일 전송이 끝난 경우 호출
+  .on('end', function() {
+    console.log('end');
+  });
+  
+  form.parse(req, function(err, fields, files) {
+    console.log('parse - ' + JSON.stringify(files), err, fields);
     
-    var form = new formidable.IncomingForm();
-    form.uploadDir = __dirname + "/../upload";
-    // 파일 전송 시 첫번째 chunk가 전달되면 호출
-    form.on('fileBegin', function(name, file) {
-      console.log('fileBegin - ' + name + ':' + JSON.stringify(file));
-    })
-    // 파일의 chunk가 전달될 때 마다 호출
-    .on('progress', function(bytesReceived, bytesExpected) {
-      console.log('progress: ' + bytesReceived + '/' + bytesExpected);
-    })
-    // 파일 전송도중 esc나 페이지 전환 등으로 중단될 때 호출
-    .on('aborted', function() {
-      console.log('aborted');
-      res.render("error");
-    })
-    // error 발생 시 호출
-    .on('error', function() {
-      console.log('error');
-      res.render("error");
-    })
-    // 파일 전송이 끝난 경우 호출
-    .on('end', function() {
-      console.log('end');
-    });
-    
-    form.parse(req, function(err, fields, files) {
-      console.log('parse - ' + JSON.stringify(files), err, fields);
+    if (err === null) {
+      var type = files.json.type;
+      var oldpath = files.json.path;
+      var filename = files.json.name;
+      var newpath = "";
       
-      if (err === null) {
-        var type = files.json.type;
-        var oldpath = files.json.path;
-        var filename = files.json.name;
-        var newpath = "";
+      if (type === "image/jpeg") {
+        newpath += __dirname + "/../public/images/cardpicture/";
+        fs.rename(oldpath, newpath + filename, function(err) {
+          if (err) {
+            res.render("error");
+          } else {
+            res.render("success");
+          }
+        });
         
-        if (type === "image/jpeg") {
-          newpath += __dirname + "/../public/images/cardpicture/";
-          fs.rename(oldpath, newpath + filename, function(err) {
-            if (err) {
-              res.render("error");
-            } else {
-              res.render("success");
-            }
-          });
+      } else if (type === "application/octet-stream" && filename.indexOf("json") > 0) {
+        newpath += __dirname + "/../db/";
+        fs.rename(oldpath, newpath + "namuami.json", function(err) {
+          /* Load DB */
+          // console.log(global.db)
+          var db = require("../db");
+          global.db = db.idols();
           
-        } else if (type === "application/octet-stream" && filename.indexOf("json") > 0) {
-          newpath += __dirname + "/../db/";
-          fs.rename(oldpath, newpath + "namuami.json", function(err) {
-            /* Load DB */
-            // console.log(global.db)
-            var db = require("../db");
-            global.db = db.idols();
-            
-            
-            if (err) {
-              res.redirect("/error");
-            } else {
-              res.redirect("/success");
-            }
-          });
-        }
+          
+          if (err) {
+            res.redirect("/error");
+          } else {
+            res.redirect("/success");
+          }
+        });
       }
-    });
-  }
+    }
+  });
+  
   
 })
 /**/
